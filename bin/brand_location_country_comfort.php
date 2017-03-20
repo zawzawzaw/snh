85e9ca12ea881a3d4ce07208a5c4b172c987b01f
shocknawe <jairus.aragon@gmail.com>
2017-03-20 14:17:27 +0800

    added hotel index country comfort

---
+++ bin/brand_location_country_comfort.php
@@ -0,0 +1,83 @@
+<?php 
+ob_start();
+?>
+<?php 
+  global $is_debug;
+  global $body_class;
+
+  // $is_debug = false;
+  $is_debug = true;
+
+  $body_class = "brand-location-page";
+?>
+<!doctype html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html lang="en" class="no-js"><!--<![endif]-->
+  <head>
+    <?php include('head_country_comfort.php'); ?>
+  </head>
+
+
+  <body class="<?php echo $body_class; ?>">
+
+    <div id="page-preloader">
+      <div id="page-preloader-line-01"></div>
+      <div id="page-preloader-line-02">
+        <div class="line-a"></div>
+        <div class="line-b"></div>
+      </div>
+    </div>
+
+
+    <!-- mobile header is outside the page wrapper -->
+    <?php include('brand_header_mobile.php'); ?>
+    
+    <div id="page-wrapper">
+      <div id="page-wrapper-content">
+
+        <?php include('brand_header_country_comfort.php'); ?>
+
+        <!--
+           ____ _____  _    ____ _____
+          / ___|_   _|/ \  |  _ \_   _|
+          \___ \ | | / _ \ | |_) || |
+           ___) || |/ ___ \|  _ < | |
+          |____/ |_/_/   \_\_| \_\|_|
+
+        -->
+
+
+
+
+        <?php include('page_brand_location_country_comfort.php'); ?>
+
+
+
+
+        <!--
+           _____ _   _ ____
+          | ____| \ | |  _ \
+          |  _| |  \| | | | |
+          | |___| |\  | |_| |
+          |_____|_| \_|____/
+
+        -->
+        
+        <?php include('js_brand_country_comfort.php'); ?>
+        <?php include('footer.php'); ?>
+
+      </div> <!-- #page-wrapper-content -->
+    </div> <!-- #page-wrapper -->
+    
+    <div id="mobile-page-hitarea"></div>
+
+  </body>
+</html>
+<?php
+// saving captured output to file
+file_put_contents('brand_location_country_comfort.html', ob_get_contents());
+// end buffering and displaying page
+ob_end_flush();
+?>
---
+++ bin/js_brand_country_comfort.php
@@ -0,0 +1,120 @@
+<?php
+  global $is_debug;
+?>
+
+<!-- 
+         _   ___     ___    ____   ____ ____  ___ ____ _____ 
+        | | / \ \   / / \  / ___| / ___|  _ \|_ _|  _ \_   _|
+     _  | |/ _ \ \ / / _ \ \___ \| |   | |_) || || |_) || |  
+    | |_| / ___ \ V / ___ \ ___) | |___|  _ < | ||  __/ | |  
+     \___/_/   \_\_/_/   \_\____/ \____|_| \_\___|_|    |_|  
+                                                             
+-->
+
+
+<?php if($is_debug): ?>
+  <!-- Development Version -->
+  <link rel="stylesheet" type="text/css" href="css/style_country_comfort.css">
+
+  <!-- Page Library -->
+  <script type="text/javascript" src="../source/js/all.js"></script>
+  <script type="text/javascript" src="../source/library/jquery/jquery-1.9.1.min.js"></script>
+  <script type="text/javascript" src="../source/library/jquery/jquery.mousewheel.min.js"></script>
+  <script type="text/javascript" src="../source/library/_js/mobile-detect.js"></script>
+  <script type="text/javascript" src="../source/library/_js/preloadjs-0.4.0.min.js"></script>
+  <script type="text/javascript" src="../source/library/gsap/TweenMax.min.js"></script>
+  <script type="text/javascript" src="../source/library/gsap/jquery.gsap.min.js"></script>
+  <script type="text/javascript" src="../source/library/gsap/easing/EasePack.min.js"></script>
+  <script type="text/javascript" src="../source/library/gsap/plugins/ScrollToPlugin.min.js"></script>
+  <script type="text/javascript" src="../source/library/scrollmagic/ScrollMagic.js"></script>
+  <script type="text/javascript" src="../source/library/scrollmagic/plugins/animation.gsap.js"></script>
+  <script type="text/javascript" src="../source/library/scrollmagic/plugins/debug.addIndicators.js"></script>
+  <script type="text/javascript" src="../source/library/scrollmagic/lib/iscroll-probe.js"></script>
+  <script type="text/javascript" src="../source/library/slick/slick.min.js"></script>
+  <script type="text/javascript" src="../source/library/instafeed/instafeed.min.js"></script>  
+  <script type="text/javascript" src="//maps.googleapis.com/maps/api/js?key=AIzaSyC2SOmCXdM5TEYYk81BFxiFwJZ_Z56moYo&v=3.exp&libraries=places"></script>
+  <script type="text/javascript" src="../source/library/infobox/infobox.js"></script>  
+  <script type="text/javascript" src="../source/library/momentjs/moment.min.js"></script>  
+  <script type="text/javascript" src="../source/library/daterangepicker/daterangepicker.js"></script>
+
+  <!-- Google Closure -->
+  <script src="../source/library/closure-library/closure/goog/base.js"></script>
+  <script src="../source/js/goog-custom-dependencies.js"></script>
+  <script type="text/javascript">
+    goog.require('sagewest.page.Brand');
+  </script>
+  <script type="text/javascript">
+    $(document).ready(function(){
+      page = new sagewest.page.Brand({});    
+    });
+
+    //Google Maps JS
+    //Set Map
+    // function initialize() {
+    //     var myLatlng = new google.maps.LatLng(-22.944736, 133.683826);
+    //     var imagePath = 'images/icons/map-pin.png'
+    //     var mapOptions = {
+    //       zoom: 5,
+    //       center: myLatlng,
+    //       mapTypeId: google.maps.MapTypeId.ROADMAP
+    //     }
+
+    //   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    //   //Callout Content
+    //   var contentString = 'Some address here..';
+    //   //Set window width + content
+    //   var infowindow = new google.maps.InfoWindow({
+    //     content: contentString,
+    //     maxWidth: 500
+    //   });
+
+    //   //Add Marker
+    //   var marker = new google.maps.Marker({
+    //     position: myLatlng,
+    //     map: map,
+    //     icon: imagePath,
+    //     title: 'image title'
+    //   });
+
+    //   google.maps.event.addListener(marker, 'click', function() {
+    //     infowindow.open(map,marker);
+    //   });
+
+    //   //Resize Function
+    //   google.maps.event.addDomListener(window, "resize", function() {
+    //     var center = map.getCenter();
+    //     google.maps.event.trigger(map, "resize");
+    //     map.setCenter(center);
+    //   });
+    // }
+
+    // google.maps.event.addDomListener(window, 'load', initialize);
+
+  </script>
+
+<?php else: ?>
+
+
+  <!-- Optimized Version -->
+  <script type="text/javascript">
+    var PAGE_GOOGLE_LIBRARY        = "js/page-google.min.js";
+    var PAGE_LIBRARY        = "js/page-libraries.min.js";
+    var PAGE_JS             = "js/page-brand.min.js";
+
+    head.load("css/style_country_comfort.css");
+    head.load(PAGE_LIBRARY, "https://maps.googleapis.com/maps/api/js?key=AIzaSyC2SOmCXdM5TEYYk81BFxiFwJZ_Z56moYo&v=3.exp", function() {
+
+      head.load(PAGE_JS, PAGE_GOOGLE_LIBRARY, function() {
+          
+        page = new sagewest.page.Brand({});
+        $('body').addClass('preload-start');
+
+      });
+    });
+
+    // start preload
+
+  </script>
+  
+
+<?php endif; ?>
